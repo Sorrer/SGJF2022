e2945f768606f00c1444158cc4190cd042f1d4e2
sorrer <sorrer@gmail.com>
2022-11-17 00:29:42 -0500

    Upgraded test scene with pretty art and mechanics

---
+++ Assets/Scripts/Game/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Game.Common.Interactable;
+using UnityEngine;
+
+namespace Game.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public GameInteractor interactor;
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(0))
+            {
+                interactor.Interact();
+            }
+        }
+    }
+}
--- Assets/Scripts/Game/Utility/TriggerTracker.cs
+++ Assets/Scripts/Game/Utility/TriggerTracker.cs
@@ -7,16 +7,21 @@ public class TriggerTracker : MonoBehaviour
 {
     public List<GameObject> colliding = new List<GameObject>();
     public bool isColliding = false; // Could just checkif colliding.size > 0, but this is cached and faster
-
+    public bool onlyCollideSolid = true;
+    
     private void OnTriggerEnter2D(Collider2D col)
     {
+        if (col.isTrigger && onlyCollideSolid) return;
+        
         colliding.Add(col.gameObject);
-
+        
         isColliding = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        if (other.isTrigger && onlyCollideSolid) return;
+        
         colliding.Remove(other.gameObject);
 
         if (colliding.Count == 0) isColliding = false;
